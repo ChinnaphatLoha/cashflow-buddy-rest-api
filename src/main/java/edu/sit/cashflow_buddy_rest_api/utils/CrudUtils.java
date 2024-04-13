9b1dd54253c1eaae294f744533546c4306309a42
ChinnaphatLoha <chinnaphat.loha@mail.kmutt.ac.th>
2024-04-13 21:29:16 +0700

    feat(utils): :sparkles: Add CrudUtils class for generic CRUD operations
    Initialized with a method named copyNonNullProperties. This method serves as a helper for the PATCH operation by copying non-null properties from the source object to the target object.

---
+++ src/main/java/edu/sit/cashflow_buddy_rest_api/utils/CrudUtils.java
@@ -0,0 +1,29 @@
+package edu.sit.cashflow_buddy_rest_api.utils;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CrudUtils {
+
+    private static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for (PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
+    public static  <T, S> void copyNonNullProperties(T source, S target) {
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+    }
+}
